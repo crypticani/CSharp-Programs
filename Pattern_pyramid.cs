46ad04bf77b9377c517bea92a74c46b8b3cb515b
Aniket Kumar <44335648+crypticani@users.noreply.github.com>
2020-08-25 19:30:29 +0530

    Add files via upload

---
+++ Pattern_pyramid.cs
@@ -0,0 +1,36 @@
+﻿/*A program to print the following pyramid pattern:
+         *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+*/
+
+using System;
+namespace myproject
+{
+    public class Pattern_pyramid
+    {
+        public static void Main(string[] args)
+        {
+            int space, rows;
+             Console.WriteLine("A program to print pyramid pattern");
+            Console.WriteLine("Enter the number of rows:");
+            rows = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<= rows; i++)
+            {
+                for (space = i; space < rows; space++)
+                {
+                    Console.Write(" ");
+                }
+             for(int star =1; star < (i*2); star++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}

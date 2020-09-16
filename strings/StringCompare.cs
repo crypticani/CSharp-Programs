e98da26b100fbab917108f179840c84f7d95e70e
Aniket Kumar <44335648+crypticani@users.noreply.github.com>
2020-09-16 18:37:49 +0530

    Create StringCompare.cs

---
+++ strings/StringCompare.cs
@@ -0,0 +1,42 @@
+/* A program in C# to compare two strings. */
+
+using System;
+namespace String
+{
+    public class StringCompare
+    {
+        public static void Main(string[] args)
+        {
+            string str1, str2;
+            int flag = 0;
+
+            Console.WriteLine("A program to compare two strings.");
+            Console.WriteLine("Enter string 1: ");
+            str1 = Console.ReadLine();
+            Console.WriteLine("Enter string 2: ");
+            str2 = Console.ReadLine();
+    
+        for(int i=0; i<str1.Length; i++)
+                {
+                    if(str1[i]!=str2[i])
+                    {
+                        flag = 0;
+                        break;
+                    }
+                    else
+                    {
+                        flag = 1;
+                    }
+                }
+            
+            if(flag==0)
+            {
+                Console.WriteLine(str1 + " and " + str2 + " are NOT equal");
+            }
+            else if(flag==1)
+            {
+                Console.WriteLine(str1 + " and " + str2 + " are Equal");
+            }
+        }
+    }
+}
